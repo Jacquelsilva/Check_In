125dd13100c8bf9415b14163a003869c0faef6cc
Jacqueline Silva <silva.jacqueline2718@gmail.com>
2026-06-10 21:08:48 -0300

    10/06/2026

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.application") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
